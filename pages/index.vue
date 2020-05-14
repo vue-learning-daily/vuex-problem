c618e52eb38a82a9e1913185492ca6e140cceb22
Manoj Kumar <napestershine@users.noreply.github.com>
2020-05-14 19:29:20 +0200

    Create index.vue

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import Banner from '@/components/home/Banner.vue'
+
+export default {
+  components: {
+    Banner
+  },
+  async fetch({ store }) {
+    await store.dispatch('post/fetchPosts', '/posts')
+  },
+  computed: {
+    ...mapGetters({
+      posts: 'post/fetchPosts'
+    })
+  }
+}
+</script>
